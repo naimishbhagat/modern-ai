dc5361c068c4a25a587be27aadb858aa5ab5aef0
Naimish Bhagat <naimish_bhagat@yahoo.com>
2026-08-19 14:01:15 +1000

    asyncio

---
+++ 13_async_python/02_async_two.py
@@ -0,0 +1,17 @@
+import asyncio
+import time
+
+async def brew(name):
+    print(f"Brewing {name}")
+    #await asyncio.sleep(2)
+    time.sleep(3)
+    print(f"{name} is ready")
+
+async def main():
+    await asyncio.gather(
+        brew("Masala chai"),
+        brew("Green chai"),
+        brew("Ginger"),
+    )
+
+asyncio.run(main())
